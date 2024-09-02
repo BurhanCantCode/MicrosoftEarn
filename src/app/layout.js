a89465505b8e51ab52145df7c647448be18748c3
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2024-09-02 20:43:27 +0000

    vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: "Microsoft Resource Teacher \n made with love by Burhan",
+  title: "Microsoft Resource Hub \n made with love by Burhan",
   description: "Microsoft Resource Hub",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
